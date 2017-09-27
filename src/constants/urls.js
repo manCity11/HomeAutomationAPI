36ebf74cf32af26d5624143f82d3d1df25a53c57
Inthalak Sirasa <inthalak.sirasa@gmail.com>
2017-09-27 19:40:06 +0200

    skeletton of the app

---
+++ src/constants/urls.js
@@ -0,0 +1,6 @@
+const host = "http://localhost:8000/";
+const API_URL = {
+    DEVICE: host.concat('devices')
+}
+
+export default API_URL;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log("test");
+import express from 'express';
+import bodyParser  from 'body-parser';
+import expressGraphQL from 'express-graphql';
+
+// Graphql schema
+//
+
+const app = express();
+
+// Middleware
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// Routing
+//
+app.get('/', (request, response) => {
+    response.end("It works!!!")
+});
+
+app.use('/graphql', expressGraphQL({
+    schema: '',
+    graphiql: true
+}));
+
+app.listen(8000, (err) => {
+    if(err) throw err;
+
+    console.log("Serser is running!!!");
+});
